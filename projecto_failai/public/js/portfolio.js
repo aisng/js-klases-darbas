1f50394d0d836daf7ff8a2de6556483f9fd3916d
aisng <tasignas@gmail.com>
2023-05-23 20:58:14 +0300

    refactoring to modules. working on portfolio

--- projecto_failai/public/js/portfolio.js
+++ projecto_failai/public/js/portfolio.js
@@ -4,12 +4,14 @@ const portfolioFilterBtnClear = document.querySelector(
   ".works-filter > button"
 );
 
-function getPortfolioJson() {
-  return fetch("/portfolio.json")
-    .then((response) => response.json())
-    .then((data) => {
-      return data;
-    });
+async function getPortfolio() {
+  try {
+    const response = await fetch("/portfolio.json");
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 }
 
 function generatePortfolioElement(data) {
@@ -28,112 +30,99 @@ function generatePortfolioElement(data) {
 
   return div;
 }
-// item.category.includes(category)
+
 async function generatePorfolio(category = null) {
   portfolioDiv.innerHTML = "";
-  let portfolioData = await getPortfolioJson();
-  // console.log(portfolioData);dokcer
+  let portfolioData = await getPortfolio();
+
   portfolioData.forEach((item) => {
     if (category == null || item.category.includes(category)) {
       let workElement = generatePortfolioElement(item);
       portfolioDiv.append(workElement);
     }
-    // let ite = item.category.split(" ");
-    // console.log("ite", ite);
-    // console.log("cat", category);
-    // if (
-    //   category == null ||
-    //   ite.includes(category) ||
-    //   ite.every(
-    //     (ele) =>
-    //       category.includes(ele) && category.every((ele) => ite.includes(ele))
-    //   )
-    // ) {
-    //   let workElement = generatePortfolioElement(item);
-    //   portfolioDiv.append(workElement);
-    // }
+  });
+}
+
+function applyPortfolioFilter() {
+  let checkedFilters = [];
+
+  $(".works-filter").on("change", (event) => {
+    let filterName = event.target.name;
+    if (event.target.checked === true && !checkedFilters.includes(filterName)) {
+      checkedFilters.push(filterName);
+    } else {
+      const index = checkedFilters.indexOf(filterName);
+      checkedFilters.splice(index, 1);
+    }
+
+    if (checkedFilters.length > 0) {
+      checkedFilters.forEach((item) => {
+        generatePorfolio(item);
+      });
+    } else {
+      generatePorfolio();
+    }
+  });
+  
+  $(".works-filter > button").on("click", (event) => {
+    event.preventDefault();
+    checkedFilters = [];
+
+    const checkboxes = document.querySelectorAll("input[type='checkbox']");
+    checkboxes.forEach((checkbox) => (checkbox.checked = false));
+    generatePorfolio();
   });
 }
 
 generatePorfolio();
+applyPortfolioFilter();
 
-let checkedFilters = [];
+//#region not working
+// const portfolioItems = document.querySelectorAll(".work > img");
+// const filterDialog = document.getElementById("filterDialog");
+// const filterColorOptions = filterDialog.querySelector("select");
 
-portfolioFilter.addEventListener("change", (event) => {
-  let filterName = event.target.name;
-  if (event.target.checked === true && !checkedFilters.includes(filterName)) {
-    checkedFilters.push(filterName);
-  } else {
-    const index = checkedFilters.indexOf(filterName);
-    checkedFilters.splice(index, 1);
-  }
+// portfolioItems.forEach((image) => {
+//   image.addEventListener("click", (event) => {
+//     console.log("kliked");
+//     const mouseX = event.clientX;
+//     const mouseY = event.clientY;
 
-  if (checkedFilters.length > 0) {
-    checkedFilters.forEach((item) => {
-      // console.log(checkedFilters);
-      generatePorfolio(item);
-    });
-    // generatePorfolio(checkedFilters);
-  } else {
-    generatePorfolio();
-  }
-});
+//     filterDialog.style.top = `${mouseY}px`;
+//     filterDialog.style.left = `${mouseX}px`;
 
-portfolioFilterBtnClear.addEventListener("click", (event) => {
-  event.preventDefault();
-  checkedFilters = [];
+//     let currentImage = event.target;
+//     // console.log(currentImage);
+//     filterDialog.show();
 
-  const checkboxes = document.querySelectorAll("input[type='checkbox']");
-  checkboxes.forEach((checkbox) => (checkbox.checked = false));
-  generatePorfolio();
-});
-
-const portfolioItems = document.querySelectorAll(".work > img");
-const filterDialog = document.getElementById("filterDialog");
-const filterColorOptions = filterDialog.querySelector("select");
-
-portfolioItems.forEach((image) => {
-  image.addEventListener("click", (event) => {
-    const mouseX = event.clientX;
-    const mouseY = event.clientY;
-
-    filterDialog.style.top = `${mouseY}px`;
-    filterDialog.style.left = `${mouseX}px`;
-
-    let currentImage = event.target;
-    // console.log(currentImage);
-    filterDialog.show();
-
-    filterColorOptions.addEventListener("change", (e) => {
-      let selectedColor = e.target.value;
-      // console.log("selectedColor before switch", selectedColor);
-      // console.log("currentImage before switch", currentImage);
-      if (currentImage !== null) {
-        console.log(currentImage);
-        switch (selectedColor) {
-          case "red":
-            currentImage.style.filter =
-              "brightness(50%) sepia(100%) hue-rotate(0deg) saturate(200%)";
-            break;
-          case "blue":
-            currentImage.style.filter =
-              "brightness(50%) sepia(100%) hue-rotate(240deg) saturate(200%)";
-            break;
-          case "green":
-            currentImage.style.filter =
-              "brightness(50%) sepia(100%) hue-rotate(120deg) saturate(200%)";
-            break;
-          default:
-            currentImage.style.filter = "none";
-            break;
-        }
-      }
-      filterDialog.close();
-      currentImage = null;
-      selectedColor = null;
-      // image.removeEventListener('click', image);
-      // console.log("selectedColor after switch", selectedColor);
-      // console.log("currentImage after switch", currentImage);
-    });
-  });
-});
+//     filterColorOptions.addEventListener("change", (e) => {
+//       let selectedColor = e.target.value;
+//       // console.log("selectedColor before switch", selectedColor);
+//       // console.log("currentImage before switch", currentImage);
+//       if (currentImage !== null) {
+//         console.log(currentImage);
+//         switch (selectedColor) {
+//           case "red":
+//             currentImage.style.filter =
+//               "brightness(50%) sepia(100%) hue-rotate(0deg) saturate(200%)";
+//             break;
+//           case "blue":
+//             currentImage.style.filter =
+//               "brightness(50%) sepia(100%) hue-rotate(240deg) saturate(200%)";
+//             break;
+//           case "green":
+//             currentImage.style.filter =
+//               "brightness(50%) sepia(100%) hue-rotate(120deg) saturate(200%)";
+//             break;
+//           default:
+//             currentImage.style.filter = "none";
+//             break;
+//         }
+//       }
+//       filterDialog.close();
+//       currentImage = null;
+//       selectedColor = null;
+//     });
+//   });
+// });
+//#endregion
